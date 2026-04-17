75b21ac883700902fc8b992a8eaf96e2d3d1bcf8
programming hero <meherun2002@gmail.com>
2026-04-17 06:45:01 +0600

    data fetched

---
+++ src/Components/homepage/FriendsCard.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const FriendsCard = ({friend}) => {
+    const {name, picture, days_since_contact, tags, status} = friend;
+
+    let statusClass = '';
+
+    if(status === 'Overdue') {
+        statusClass = 'bg-red-500 text-white';
+    }
+    else if(status === 'Almost Due') {
+        statusClass = 'bg-orange-500 text-white';
+    }
+    else if(status === 'On-Track') {
+        statusClass = 'bg-green-900 text-white';
+    }
+    return (
+        <div className='rounded-2xl shadow-sm border border-gray-100 p-6 flex flex-col items-center text-center transition-all hover:shadow-md'>
+            <img className='w-20 h-20 rounded-full object-cover border-2 border-gray-50 shadow-inner' src={picture} alt={name} />
+            <h3 className='text-lg font-bold text-gray-800 mb-1'>{name}</h3>
+            <p className='text-gray-400 text-sm font-medium mb-3'>{days_since_contact}d ago</p>
+
+            <div className='flex flex-wrap justify-center gap-2'>
+                {tags.map((tag, index) => (
+                    <span className=' px-3 py-1 bg-green-300 font-bold uppercase rounded-full text-[11px]' key={index}>
+                        {tag}
+                    </span>
+                ))}
+            </div>
+
+            <div className={`mt-3 px-3 py-1 rounded-xl text-[11px] font-bold shadow-sm ${statusClass}`}>
+                {status}
+            </div>
+        </div>
+    );
+};
+
+export default FriendsCard;
--- src/Components/homepage/YourFriends.jsx
+++ src/Components/homepage/YourFriends.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import FriendsCard from './FriendsCard';
 
-const YourFriends = () => {
+const FriendsCards = () => {
+    const [friends, setFriends] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() =>{
+        fetch('/friends.json')
+            .then(res => res.json())
+            .then(data => {
+                setFriends(data)
+                setLoading(false);
+            })
+    }, []);
+
+    if (loading) {
+        return <span className="loading loading-bars loading-xl"></span>
+    }
+
     return (
-        <div>
-            your friends
+        <div className='max-w-5xl mx-auto p-10'>
+            <h2 className='text-2xl font-bold mb-6 text-gray-800'>Your Friends</h2>
+
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
+                {friends.map(friend => <FriendsCard key={friend.id} friend={friend}></FriendsCard>)}
+            </div>
         </div>
     );
 };
 
-export default YourFriends;
+export default FriendsCards;
